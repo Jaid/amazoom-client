a86d99a88f4a0d5f865bd4ac5c692952c7471ecc
Jaid <jaid.jsx@gmail.com>
2019-08-22 03:48:19 +0200

    manage: Added analytics ID
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ import configure from "webpack-config-jaid"
 
 export default configure({
   publishimo: {fetchGithub: true},
-  googleAnalyticsTrackingId: "INSERT",
+  googleAnalyticsTrackingId: "UA-51563406-8",
   robots: true,
   icon: path.join(__dirname, "icon.png"),
   backgroundColor: "#580000",
